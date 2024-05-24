c590afaadd9fadd722ec357bee43eedbe9f90b9f
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-05-24 15:01:43 +0300

    add props to sheet file

--- src/app/(main)/agency/[agencyId]/layout.tsx
+++ src/app/(main)/agency/[agencyId]/layout.tsx
@@ -51,7 +51,9 @@ const layout = async ({ children, params }: Props) => {
           role={allNoti.User?.role}
         />
         <div className="relative">
-          <BlurPage>{children}</BlurPage>
+          <BlurPage>
+            {children}
+          </BlurPage>
         </div>
       </div>
     </div>
