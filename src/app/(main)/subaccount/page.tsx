5607f004ba089dca579d391bd8700a862cba1a4c
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-05-31 15:40:15 +0300

    work on the sub account section

--- src/app/(main)/subaccount/page.tsx
+++ src/app/(main)/subaccount/page.tsx
@@ -39,13 +39,39 @@
 
 // export default SubAccountMainPage
 
+import Unauthorized from "@/components/unauthorized";
+import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
+import { redirect } from "next/navigation";
+import React from "react";
 
-import React from 'react'
+type Props = {
+  searchParams: { state: string; code: string };
+};
 
-const SubAccountMainPage = () => {
-  return (
-    <div>SubAccountMainPage</div>
-  )
-}
+const SubAccountMainPage = async ({ searchParams }: Props) => {
+  const agencyId = await verifyAndAcceptInvitation();
 
-export default SubAccountMainPage
+  if (!agencyId) {
+    return <Unauthorized />;
+  }
+
+  const user = await getAuthUserDetails();
+  if (!user) return;
+
+  const getFirstSubaccountWithAccess = user.Permissions.find(
+    (permission) => permission.access === true
+  );
+
+  if (searchParams.state) {
+    const statePath = searchParams.state.split('___')[0]
+    const stateSubaccountId = searchParams.state.split('___')[1]
+    if (!stateSubaccountId) return <Unauthorized />
+    return redirect(
+      `/subaccount/${stateSubaccountId}/${statePath}?code=${searchParams.code}`
+    )
+  }
+
+  return <div>SubAccountMainPage</div>;
+};
+
+export default SubAccountMainPage;
