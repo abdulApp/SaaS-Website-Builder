24ea2c47aeaf9fd926b88d67ec5520e7ad6fd1cc
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-06-04 07:13:13 +0300

    working on pipeline ticket details and contact has a bug

--- src/app/(main)/subaccount/[subaccountId]/settings/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/settings/page.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import SubAccountDetails from "@/components/forms/subaccount-details";
+import UserDetails from "@/components/forms/user-details";
+import BlurPage from "@/components/global/blur-page";
+import { db } from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
+import React from "react";
 
-const Settings = () => {
+type Props = {
+  params: { subaccountId: string };
+};
+
+const SubaccountSettingPage = async ({ params }: Props) => {
+  const authUser = await currentUser();
+  if (!authUser) return;
+  const userDetails = await db.user.findUnique({
+    where: {
+      email: authUser.emailAddresses[0].emailAddress,
+    },
+  });
+  if (!userDetails) return;
+
+  const subAccount = await db.subAccount.findUnique({
+    where: { id: params.subaccountId },
+  });
+  if (!subAccount) return;
+
+  const agencyDetails = await db.agency.findUnique({
+    where: { id: subAccount.agencyId },
+    include: { SubAccount: true },
+  });
+
+  if (!agencyDetails) return;
+  const subAccounts = agencyDetails.SubAccount;
+  
   return (
-    <div>Settings</div>
-  )
-}
+    <BlurPage>
+      <div className="flex lg:!flex-row flex-col gap-4">
+        <SubAccountDetails
+          agencyDetails={agencyDetails}
+          details={subAccount}
+          userId={userDetails.id}
+          userName={userDetails.name}
+        />
+        <UserDetails
+          type="subaccount"
+          id={params.subaccountId}
+          subAccounts={subAccounts}
+          userData={userDetails}
+        />
+      </div>
+    </BlurPage>
+  );
+};
 
-export default Settings
+export default SubaccountSettingPage;
