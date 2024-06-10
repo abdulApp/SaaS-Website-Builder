05bb3dcc158b5e3e903744fb380f49bc5aca2094
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2024-06-11 00:26:30 +0300

    save it

--- src/app/(main)/subaccount/[subaccountId]/funnels/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { getFunnels } from "@/lib/queries";
+import React from "react";
+import FunnelsDataTable from "./data-table";
+import { Plus } from "lucide-react";
+import { columns } from "./columns";
+import FunnelForm from "@/components/forms/funnel-form";
+import BlurPage from "@/components/global/blur-page";
 
-const Funnels = () => {
+const Funnels = async ({ params }: { params: { subaccountId: string } }) => {
+  const funnels = await getFunnels(params.subaccountId);
+  if (!funnels) return null;
+
   return (
-    <div>Funnels</div>
-  )
-}
+    <BlurPage>
+      <FunnelsDataTable
+        actionButtonText={
+          <>
+            <Plus size={15} />
+            Create Funnel
+          </>
+        }
+        modalChildren={
+          //@ts-ignore
+          <FunnelForm subAccountId={params.subaccountId}></FunnelForm>
+        }
+        filterValue="name"
+        columns={columns}
+        data={funnels}
+      />
+    </BlurPage>
+  );
+};
 
-export default Funnels
+export default Funnels;
